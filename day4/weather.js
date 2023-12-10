5b6584b02dd05294902f39194c1aaba19c1d1112
H1519 <viethungn758@gmail.com>
2023-12-10 21:35:46 +0700

    xong bt

--- day4/weather.js
+++ day4/weather.js
@@ -11,20 +11,51 @@ fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${input_place}`)
   .then(function (data) {
     console.log(data);
     let latitude = data.results[0].latitude;
-    let longitude=data.results[0].longtitude;
+    let longitude=data.results[0].longitude;
     fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
       .then(function (res) {
         return res.json();
       })
       .then(function (weather_data) {
         console.log(weather_data);
-      });
-    // truy vấn để lấy latitude và longitude
-     asd = `<h5>Tọa độ:
-     ${data.results[0].latitude}
+      // truy vấn để lấy latitude và longitude
+     
+    
+    console.log(weather_data.current_weather.weathercode);
+    let weathercode = weather_data.current_weather.weathercode
+    if(weathercode <=3){
+      asd = `<h5>Tọa độ:
+     ${data.results[0].latitude} ;
      ${data.results[0].longitude}</h5>
      <h5>Nhiệt độ:
-     ${data.current_weather.temperature}</h5>`
-    div.innerHTML = asd;
-})
+     ${weather_data.current_weather.temperature}</h5>
+     <img src="https://i.pinimg.com/originals/90/85/bb/9085bb2580eee46db433164fdcca9500.gif" alt="" style="width:200px;height: 200px;">`
+    }
+    if(weathercode >= 48 && weathercode<=67){
+      asd = `<h5>Tọa độ:
+     ${data.results[0].latitude} ;
+     ${data.results[0].longitude}</h5>
+     <h5>Nhiệt độ:
+     ${weather_data.current_weather.temperature}</h5>
+     <img src="https://i.pinimg.com/originals/89/27/01/892701252dab9ead045f745d999cf9fc.gif" alt="" style="width:200px;height: 200px;">`
+    }
+    if(weathercode >= 71 && weathercode<=86){
+      asd = `<h5>Tọa độ:
+     ${data.results[0].latitude} ;
+     ${data.results[0].longitude}</h5>
+     <h5>Nhiệt độ:
+     ${weather_data.current_weather.temperature}</h5>
+     <img src="https://i.pinimg.com/originals/94/0f/42/940f42ee6c416688121156ced1fadb66.gif" alt="" style="width:200px;height: 200px;">`
+    }
+    if(weathercode >= 95 && weathercode<=99){
+      asd = `<h5>Tọa độ:
+     ${data.results[0].latitude} ;
+     ${data.results[0].longitude}</h5>
+     <h5>Nhiệt độ:
+     ${weather_data.current_weather.temperature}</h5>
+     <img src="https://i.pinimg.com/originals/76/e3/f8/76e3f8a2710800715368f40bcded954f.gif" alt="" style="width:200px;height: 200px;">`
+    }
+div.innerHTML = asd;
+});
+});
 })
